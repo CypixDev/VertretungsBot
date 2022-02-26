fe45c5639e8bcdcedc1710cb989829896d1bc3dc
CypixDev <cypixzockt@gmail.com>
2022-02-26 21:03:25 +0100

    Added new command to get notify list

---
+++ src/main/java/de/cypix/vertretungsplanbot/bot/commands/cmds/CMDNotifyList.java
@@ -0,0 +1,27 @@
+package de.cypix.vertretungsplanbot.bot.commands.cmds;
+
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
+import de.cypix.vertretungsplanbot.bot.commands.TelegramCommand;
+import de.cypix.vertretungsplanbot.bot.inlinekeyboardcallback.KeyboardCallBackBuilder;
+import de.cypix.vertretungsplanbot.bot.inlinekeyboardcallback.KeyboardCallbackType;
+import de.cypix.vertretungsplanbot.main.VertretungsPlanBot;
+import de.cypix.vertretungsplanbot.sql.SQLManager;
+
+public class CMDNotifyList implements TelegramCommand {
+    @Override
+    public void performCommand(User user, Chat chat, Message message, String[] args) {
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
+        for (String className : SQLManager.getAllNotifiesByChatId(chat.id())) {
+            inlineKeyboard.addRow(new InlineKeyboardButton(className).callbackData(
+                    new KeyboardCallBackBuilder(KeyboardCallbackType.NOTIFY, "overview").addData("class", className).build()));
+            System.out.println("....");
+        }
+
+        VertretungsPlanBot.getBot().execute(new SendMessage(chat.id(), "Hier die Liste deiner Notifications:").replyMarkup(inlineKeyboard));
+    }
+}
