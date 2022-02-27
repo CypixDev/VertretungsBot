c2783047f2a1fc9a563703565259490a017df13e
CypixDev <cypixzockt@gmail.com>
2022-02-27 13:26:13 +0100

    Added new command

---
+++ src/main/java/de/cypix/vertretungsplanbot/bot/commands/cmds/CMDStatus.java
@@ -0,0 +1,20 @@
+package de.cypix.vertretungsplanbot.bot.commands.cmds;
+
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.request.SendMessage;
+import de.cypix.vertretungsplanbot.bot.commands.TelegramCommand;
+import de.cypix.vertretungsplanbot.main.VertretungsPlanBot;
+
+public class CMDStatus implements TelegramCommand {
+    @Override
+    public void performCommand(User user, Chat chat, Message message, String[] args) {
+        VertretungsPlanBot.getBot().execute(new SendMessage(chat.id(), "Status der Systeme:" +
+                "" +
+                "Telegrambot ✅\n" +
+                "Datenbank "+(VertretungsPlanBot.getSqlConnector().isConnected() ? "✅" : "❌")+"\n" +
+                "Discord "+"❌\n" +
+                "Signal "+"❌\n"));
+    }
+}
