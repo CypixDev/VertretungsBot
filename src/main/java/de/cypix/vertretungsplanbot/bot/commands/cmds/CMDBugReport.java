f4327f485cab88148d0748e3237d84375a8834e3
CypixDev <cypixzockt@gmail.com>
2022-03-01 21:03:30 +0100

    New command

---
+++ src/main/java/de/cypix/vertretungsplanbot/bot/commands/cmds/CMDBugReport.java
@@ -0,0 +1,13 @@
+package de.cypix.vertretungsplanbot.bot.commands.cmds;
+
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.User;
+import de.cypix.vertretungsplanbot.bot.commands.TelegramCommand;
+
+public class CMDBugReport implements TelegramCommand {
+    @Override
+    public void performCommand(User user, Chat chat, Message message, String[] args) {
+        chat.
+    }
+}
