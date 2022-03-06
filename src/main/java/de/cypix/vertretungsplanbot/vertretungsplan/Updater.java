e3e463067d152d0de5993caafb84eb03988064db
CypixDev <cypixzockt@gmail.com>
2022-03-07 00:53:33 +0100

    Implementing new structure and simulation

--- src/main/java/de/cypix/vertretungsplanbot/vertretungsplan/Updater.java
+++ src/main/java/de/cypix/vertretungsplanbot/vertretungsplan/Updater.java
@@ -21,13 +21,17 @@ public class Updater extends Thread {
     private static LocalDateTime lastRefreshTimeStamp = null;
     private static LocalDate representationDate = null;
 
+    public static boolean simulateLoss = false;
+    public static boolean simulateUpdate = false;
+    public static boolean simulateAdd = false;
+
     private static final boolean DEBUG = false;
 
     @Override
     public void run() {
         try {
             while (keepRunning()) {
-                if(LocalDateTime.now().getHour() < 23 && LocalDateTime.now().getHour() >= 1){
+                if(/*LocalDateTime.now().getHour() < 23 && LocalDateTime.now().getHour() >= 1*/true){
                     try {
                         URL url = new URL("https://btr-rs.de/btr-old/service-vertretungsplan.php");
                         Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
@@ -43,6 +47,13 @@ public class Updater extends Thread {
                      */
                         List<VertretungsEntry> newEntries = filter(scanner);
                         List<VertretungsEntry> oldEntries = SQLManager.getAllRelevantEntries();
+                        List<VertretungsEntry> oldEntriesLeft = new ArrayList<>(oldEntries);
+
+                        /*Konzept wegfallende einträge prüfen
+                            Die alten Einträge durchgehen und alle löschen die auch im neune vorkommen
+                          ODER
+                            Die Neue Liste durchgehen und schauen welche der Alten einträge nicht mehr drinnen sind
+                         */
 
                         //TODO: check if something is deleted
                         if (newEntries.size() == oldEntries.size()) {
@@ -51,10 +62,20 @@ public class Updater extends Thread {
                             System.out.println("Es gibt neue einträge!");
                         } else {
                             System.out.println("Es sind weniger einträge...");
+                            //TODO: Check lost entries
                         }
 
+
+                        if(simulateAdd){
+                            newEntries.add(new VertretungsEntry(LocalDateTime.now(), LocalDate.now(), "TEST", "XXXX", "YYYY", "HHAA", "KAKA"));
+                        }
                         for (VertretungsEntry newEntry : newEntries) {
                             //If none fits it is probably a new entry
+                            if(simulateUpdate){
+                                newEntry.getLastEntryUpdate().setTeacherLong("LEEELLLL");
+                                newEntry.getLastEntryUpdate().setTeacherShort("DE");
+                                simulateUpdate = false;
+                            }
                             boolean exist = false;
                             for (VertretungsEntry oldEntry : oldEntries) {
                                 switch (newEntry.compareTo(oldEntry)) {
@@ -64,14 +85,18 @@ public class Updater extends Thread {
                                             2 defaults same
                                          */
                                     case 0: //same
+                                        if(!simulateLoss){
+                                            oldEntriesLeft.remove(oldEntry);
+                                        }else simulateLoss = false;
                                         exist = true;
+
                                         //Nothing to do....
                                         break;
                                     case 1: //different
                                         //could be other one or not existing
                                         //Nothing to do....
                                         break;
-                                    case 2://Aktuallisieren
+                                    case 2://Aktuallisieren -- Defaults same but not defaults not
                                     /*TODO: check....
                                     if(!getLastRefresh(scanner).format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"))
                                             .equals(SQLManager.getLastRegisteredRefresh().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")))){
@@ -80,19 +105,30 @@ public class Updater extends Thread {
                                     System.out.println(oldEntry.toString());
                                     System.out.println(newEntry.toString());
                                     System.out.println();*/
+
                                         exist = true;
-                                        SQLManager.updateEntry(newEntry);
+                                        oldEntriesLeft.remove(oldEntry);
+                                        SQLManager.insertNewUpdate(oldEntry.getEntryId(), newEntry.getLastEntryUpdate());
+                                        //SQLManager.updateEntry(newEntry);
+                                        //TODO: Check what has changed and change it
                                         if(DEBUG) System.out.println("entry updated!");
                                         /*for (Long allChatIDsByNotifyClass : SQLManager.getAllChatIDsByNotifyClass(newEntry.getClassName())) {
                                             VertretungsPlanBot.getBot().execute(new SendMessage(allChatIDsByNotifyClass, "Änderung in "+newEntry.getNewSubject()+" und so weiter...."));
-                                        }
+                                        }*/
+                                        VertretungsPlanBot.getBot().execute(new SendMessage(259699517,"Aktuallisierung für:\n" +
+                                                "Datum: "+newEntry.getRepresentationDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))+"\n" +
+                                                "Klasse: "+newEntry.getClassName()+"\n" +
+                                                "Stunden: "+newEntry.getDefaultHour()));
 
-                                         */
                                         break;
                                 }
                             }
                             if (!exist) {
-                                SQLManager.insertNewEntry(newEntry);
+
+                                if(!newEntry.getClassName().equals("TEST")){ //For simulation....
+                                    SQLManager.insertNewEntry(newEntry);
+                                }
+                                System.out.println("Added new entry!");
                                 for (Long allChatIDsByNotifyClass : SQLManager.getAllChatIDsByNotifyClass(newEntry.getClassName())) {
                                     StringBuilder builder = new StringBuilder();
                                     builder.append("Neuer eintrag für den ")
@@ -101,7 +137,7 @@ public class Updater extends Thread {
 
                                     builder.append("Klasse: ").append(newEntry.getClassName()).append("\n");
                                     builder.append("Stunde: ").append(newEntry.getDefaultHour()).append("\n");
-                                    builder.append("Fach: ").append(newEntry.getDefaultSubject()).append("\n");
+                                    builder.append("Fach: ").append(newEntry.getDefaultSubject()).append("\n");/*
                                     if(newEntry.getNote() != null && !newEntry.getNote().equals("null"))
                                         builder.append("Anmerkung: ").append(newEntry.getNote()).append("\n");
                                     if(newEntry.getNewTeacher() != null && !newEntry.getNewTeacher().equals("null"))
@@ -111,11 +147,21 @@ public class Updater extends Thread {
                                     if(newEntry.getNewRoom() != null && !newEntry.getNewRoom().equals("null"))
                                         builder.append("Neuer Raum: ").append(newEntry.getNewRoom()).append("\n");
                                     if(newEntry.getNewHour() != null && !newEntry.getNewHour().equals("null"))
-                                        builder.append("Neue Stunde: ").append(newEntry.getNewHour()).append("\n");
+                                        builder.append("Neue Stunde: ").append(newEntry.getNewHour()).append("\n");*/
 
-
-                                    VertretungsPlanBot.getBot().execute(new SendMessage(allChatIDsByNotifyClass,builder.toString()));
+                                    if(!VertretungsPlanBot.getConfigManager().isMaintenance() || allChatIDsByNotifyClass == 259699517)
+                                        VertretungsPlanBot.getBot().execute(new SendMessage(allChatIDsByNotifyClass,builder.toString()));
                                 }
+                            }
+                        }
+                        if(!oldEntriesLeft.isEmpty()){
+                            System.out.println("Folgende Entries sind weggefallen:");
+                            for (VertretungsEntry entry : oldEntriesLeft) {
+                                System.out.println("- "+entry.getEntryId());
+                                VertretungsPlanBot.getBot().execute(new SendMessage(259699517,"Eintrag weggefallen:\n" +
+                                        "Datum: "+entry.getRepresentationDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))+"\n" +
+                                        "Klasse: "+entry.getClassName()+"\n" +
+                                        "Stunden: "+entry.getDefaultHour()));
                             }
                         }
 
@@ -144,6 +190,13 @@ public class Updater extends Thread {
 
     private synchronized boolean keepRunning() {
         return !this.stop;
+    }
+
+    private boolean containsEntry(List<VertretungsEntry> list, VertretungsEntry entryCheck){
+        for (VertretungsEntry entry : list) {
+            if(entry.compareTo(entryCheck) == 1) return true;
+        }
+        return false;
     }
 
     public static List<VertretungsEntry> filter(Scanner scanner) {
@@ -185,6 +238,7 @@ public class Updater extends Thread {
                 line = line.replace("&nbsp;", " ");
                 line = line.replace("&ouml;", "ö");
                 line = line.replace("&auml;", "ä");
+                line = line.replace("&uuml;", "ü");
                 line = line.replace("<td style='padding: 2px; border: 1px solid #000000;'>", "");
                 line = line.replace("</td>", "");
                 if(DEBUG) System.out.println("Lehrer: " + line);
@@ -235,6 +289,7 @@ public class Updater extends Thread {
                     line = line.replace("&nbsp;", " ");
                     line = line.replace("&ouml;", "ö");
                     line = line.replace("&auml;", "ä");
+                    line = line.replace("&uuml;", "ü");
                     line = line.replace("<td style='font-weight: bold; padding: 2px; border: 1px solid #000000;'>", "");
                     line = line.replace("</td>", "");
                     if(DEBUG) System.out.println("Lehrer: " + line);
@@ -254,19 +309,27 @@ public class Updater extends Thread {
                     if(DEBUG) System.out.println("Grund: " + line);
 
                     //Adding entry full
+
                     VertretungsEntry entry = new VertretungsEntry(LocalDateTime.now(),
-                            lastRefreshTimeStamp,
                             representationDate,
                             defaultClass,
                             defaultHour,
                             defaultRoom,
                             defaultTeacher,
-                            defaultSubject,
-                            note,
-                            newHour,
-                            newRoom,
-                            newTeacher,
-                            newSubject);
+                            defaultSubject);
+                    VertretungsEntryUpdate entryUpdate = new VertretungsEntryUpdate(entry, LocalDateTime.now());
+                    entryUpdate.setNote(note);
+                    entryUpdate.setHour(newHour);
+                    entryUpdate.setRoom(newRoom);
+                    //Check is needed her because of split.... Others don't need because 'null' will be filtered out...
+                    if(!newTeacher.equalsIgnoreCase("null") && !newTeacher.equalsIgnoreCase("") && !newTeacher.equalsIgnoreCase(" ")){
+                        entryUpdate.setTeacherLong(newTeacher.split(" ")[1].replace("(", "")+" "+newTeacher.split(" ")[2].replace(")", ""));
+                        entryUpdate.setTeacherShort(newTeacher.split(" ")[0]);
+                    }
+                    entryUpdate.setSubject(newSubject);
+
+                    entry.setLastEntryUpdate(entryUpdate);
+
                     list.add(entry);
 
                 }
--- src/main/java/de/cypix/vertretungsplanbot/vertretungsplan/VertretungsEntry.java
+++ src/main/java/de/cypix/vertretungsplanbot/vertretungsplan/VertretungsEntry.java
@@ -1,70 +1,72 @@
 package de.cypix.vertretungsplanbot.vertretungsplan;
 
+import de.cypix.vertretungsplanbot.sql.SQLManager;
+import de.cypix.vertretungsplanbot.sql.UpdateType;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class VertretungsEntry implements Comparable<VertretungsEntry> {
+
     private int entryId;
     private final LocalDateTime registrationTimeStamp;
-    private final LocalDateTime lastRefreshTimeStamp;
     private final LocalDate representationDate;
     private final String className;
     private final String defaultHour;
     private final String defaultRoom;
     private final String defaultTeacher;
     private final String defaultSubject;
-    private final String note;
-    private String newHour;
-    private String newRoom;
-    private String newTeacher;
-    private String newSubject;
+    private VertretungsEntryUpdate lastEntryUpdate;
 
-    public VertretungsEntry(int entryId, LocalDateTime registrationTimeStamp, LocalDateTime lastRefreshTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject, String note, String newHour, String newRoom, String newTeacher, String newSubject) {
+
+    public VertretungsEntry(int entryId, LocalDateTime registrationTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject, VertretungsEntryUpdate lastEntryUpdate) {
+        this.entryId = entryId;
+        this.registrationTimeStamp = registrationTimeStamp;
+        this.representationDate = representationDate;
+        this.className = className;
+        this.defaultHour = defaultHour;
+        this.defaultRoom = defaultRoom;
+        this.defaultTeacher = defaultTeacher;
+        this.defaultSubject = defaultSubject;
+        this.lastEntryUpdate = lastEntryUpdate;
+    }
+
+    public VertretungsEntry(int entryId, LocalDateTime registrationTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject) {
         this.entryId = entryId;
         this.registrationTimeStamp = LocalDateTime.parse(registrationTimeStamp.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
-        this.lastRefreshTimeStamp = lastRefreshTimeStamp;
         this.representationDate = representationDate;
         this.className = className;
         this.defaultHour = defaultHour;
         this.defaultRoom = defaultRoom;
         this.defaultTeacher = defaultTeacher;
         this.defaultSubject = defaultSubject;
-        this.note = note;
-        this.newHour = newHour;
-        this.newRoom = newRoom;
-        this.newTeacher = newTeacher;
-        this.newSubject = newSubject;
     }
 
-    public VertretungsEntry(LocalDateTime registrationTimeStamp, LocalDateTime lastRefreshTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject, String note, String newHour, String newRoom, String newTeacher, String newSubject) {
+    public VertretungsEntry(LocalDateTime registrationTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject) {
         this.registrationTimeStamp = LocalDateTime.parse(registrationTimeStamp.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
-        this.lastRefreshTimeStamp = lastRefreshTimeStamp;
         this.representationDate = representationDate;
         this.className = className;
         this.defaultHour = defaultHour;
         this.defaultRoom = defaultRoom;
         this.defaultTeacher = defaultTeacher;
         this.defaultSubject = defaultSubject;
-        this.note = note;
-        this.newHour = newHour;
-        this.newRoom = newRoom;
-        this.newTeacher = newTeacher;
-        this.newSubject = newSubject;
     }
 
-    public VertretungsEntry(LocalDateTime registrationTimeStamp, LocalDateTime lastRefreshTimeStamp, LocalDate representationDate, String className, String defaultHour, String defaultRoom, String defaultTeacher, String defaultSubject, String note) {
-        this.registrationTimeStamp = LocalDateTime.parse(registrationTimeStamp.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
-        this.lastRefreshTimeStamp = lastRefreshTimeStamp;
-        this.representationDate = representationDate;
-        this.className = className;
-        this.defaultHour = defaultHour;
-        this.defaultRoom = defaultRoom;
-        this.defaultTeacher = defaultTeacher;
-        this.defaultSubject = defaultSubject;
-        this.note = note;
+    //Just use on insert....
+    public void insertAllNewUpdates() {
+        //load entryId
+        //TODO: Check if it's working fine....
+        entryId = SQLManager.getLastInsertedEntryId();
+        System.out.print("Id: "+entryId);
+/*        if(getNote() != null) SQLManager.insertNewUpdate(UpdateType.NOTE, entryId, note);
+        if(getNewHour() != null) SQLManager.insertNewUpdate(UpdateType.HOUR, entryId, newHour);
+        if(getNewRoom() != null) SQLManager.insertNewUpdate(UpdateType.ROOM, entryId, newRoom);
+        if(getNewTeacher() != null) SQLManager.insertNewUpdate(UpdateType.TEACHER, entryId, getNewTeacherShort()+"__"+getNewTeacherLong());
+        if(getNewSubject() != null) SQLManager.insertNewUpdate(UpdateType.SUBJECT, entryId, newSubject);*/
+
     }
 
     public int getEntryId() {
@@ -73,10 +75,6 @@ public class VertretungsEntry implements Comparable<VertretungsEntry> {
 
     public LocalDateTime getRegistrationTimeStamp() {
         return registrationTimeStamp;
-    }
-
-    public LocalDateTime getLastRefreshTimeStamp() {
-        return lastRefreshTimeStamp;
     }
 
     public LocalDate getRepresentationDate() {
@@ -95,55 +93,41 @@ public class VertretungsEntry implements Comparable<VertretungsEntry> {
         return defaultRoom;
     }
 
+    @Deprecated
     public String getDefaultTeacher() {
         return defaultTeacher;
     }
 
+    public String getDefaultTeacherShort() {
+        return defaultTeacher.split(" ")[0];
+    }
+
+    public String getDefaultTeacherLong() {
+        return defaultTeacher.split(" ")[1].replace("(", "")+" "+defaultTeacher.split(" ")[2].replace(")", "");
+    }
     public String getDefaultSubject() {
         return defaultSubject;
     }
 
-    public String getNote() {
-        if (note.equals("") || note.equals(" ") || note.equals("..")) {
-            return null;
-        }
-        return note;
+
+    public void setEntryId(int entryId) {
+        this.entryId = entryId;
     }
 
-    public String getNewHour() {
-        if (newHour.equals("") || newHour.equals(" ") || newHour.equals("..")) {
-            return null;
-        }
-        return newHour;
+    public VertretungsEntryUpdate getLastEntryUpdate() {
+        return lastEntryUpdate;
     }
 
-    public String getNewRoom() {
-        if (newRoom.equals("") || newRoom.equals(" ") || newRoom.equals("..")) {
-            return null;
-        }
-        return newRoom;
+    public void setLastEntryUpdate(VertretungsEntryUpdate lastEntryUpdate) {
+        this.lastEntryUpdate = lastEntryUpdate;
     }
 
-    public String getNewTeacher() {
-        if (newTeacher.equals("") || newTeacher.equals(" ") || newTeacher.equals("..")) {
-            return null;
-        }
-        return newTeacher;
-    }
+    /* NOT Up to date, I think
+0 means same
+1 absolut different
+2 defaults same
+             */
 
-    public String getNewSubject() {
-        if (newSubject.equals("") || newSubject.equals(" ") || newSubject.equals("..")) {
-            return null;
-        }
-        return newSubject;
-    }
-
-
-    /*
-    -1 means different
-    0 means same
-    1 means default is same
-     */
     @Override
     public int compareTo(@NotNull VertretungsEntry o) {
         boolean defaultSame = getRepresentationDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
@@ -151,25 +135,17 @@ public class VertretungsEntry implements Comparable<VertretungsEntry> {
                 getClassName().replace(" ", "").equals(o.getClassName().replace(" ", "")) &&
                 getDefaultHour().replace(" ", "").equals(o.getDefaultHour().replace(" ", "")) &&
                 getDefaultRoom().replace(" ", "").equals(o.getDefaultRoom().replace(" ", ""));
-/*        System.out.println(getRepresentationDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-                +" equals "+(o.getRepresentationDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))) +"\n && "+
-                getClassName().replace(" ", "")+" equals "+(o.getClassName().replace(" ", "")) +"\n && "+
-                getDefaultHour().replace(" ", "")+" equals "+(o.getDefaultHour().replace(" ", "")) +"\n && "+
-                getDefaultRoom().replace(" ", "")+" equals "+(o.getDefaultRoom().replace(" ", ""))+"====> "+defaultSame+"\n\n");
-        *///Hopefully unique identification
-        if (defaultSame && (getNote() != null && o.getNote() != null && getNote().equals(o.getNote()))) {
-            if (!(getNewHour() != null && o.getNewHour() != null && getNewHour().equals(o.getNewHour())))
+            /*DEBUG STUFF
+            System.out.println("------------------------");
+            System.out.println("New:"+toString());
+            System.out.println("Old:"+o.toString());
+            System.out.println("------------------------");
+            */
+        if (defaultSame) {
+            if(getLastEntryUpdate() != null && o.getLastEntryUpdate() != null && getLastEntryUpdate().compareTo(o.getLastEntryUpdate()) != 0){
                 return 2;
-            if (!(getNewRoom() != null && o.getNewRoom() != null && getNewRoom().equals(o.getNewRoom())))
-                return 2;
-            if (!(getNewTeacher() != null && o.getNewTeacher() != null && getNewTeacher().equals(o.getNewTeacher())))
-                return 2;
-            if (!(getNewSubject() != null && o.getNewSubject() != null && getNewSubject().equals(o.getNewSubject())))
-                return 2;
-
+            }
             return 0;
-        } else if (defaultSame) {
-            return 2;
         } else return 1;
     }
 
@@ -178,18 +154,22 @@ public class VertretungsEntry implements Comparable<VertretungsEntry> {
         return "VertretungsEntry{" +
                 "entryId=" + entryId +
                 ", registrationTimeStamp=" + registrationTimeStamp +
-                ", lastRefreshTimeStamp=" + lastRefreshTimeStamp +
+                ", representationDate=" + representationDate +
+                ", className='" + className + '\'' +
+                ", defaultHour='" + defaultHour + '\'' +
+                ", defaultRoom='" + defaultRoom + '\'' +
+                ", defaultTeacherShort='" + getDefaultTeacherShort() + '\'' +
+                ", defaultSubject='" + defaultSubject + '\'' +
+                '}';
+    }
+    public String toStringDefaults(){
+        return "VertretungsEntry{" +
                 ", representationDate=" + representationDate +
                 ", className='" + className + '\'' +
                 ", defaultHour='" + defaultHour + '\'' +
                 ", defaultRoom='" + defaultRoom + '\'' +
                 ", defaultTeacher='" + defaultTeacher + '\'' +
-                ", defaultSubject='" + defaultSubject + '\'' +
-                ", note='" + note + '\'' +
-                ", newHour='" + newHour + '\'' +
-                ", newRoom='" + newRoom + '\'' +
-                ", newTeacher='" + newTeacher + '\'' +
-                ", newSubject='" + newSubject + '\'' +
-                '}';
+                ", defaultSubject='" + defaultSubject + '\'';
     }
+
 }
